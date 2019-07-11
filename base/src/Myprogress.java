8007df15d237030ab9e9b376a2f0f8e082ef824f
AlexWuDDD <wkjworking@outlook.com>
2019-07-11 08:57:34 +0800

    2019-7-11

--- base/src/Myprogress.java
+++ base/src/Myprogress.java
@@ -9,7 +9,8 @@ public class Myprogress
         System.out.println("Tue Jul 09 15:58:01 CST 2019" + " 看到 4.4， 还没开始");
         System.out.println("Wed Jul 10 06:52:20 CST 2019" + " 看到 4.6， 还没开始");
         System.out.println("Wed Jul 10 09:19:52 CST 2019" + " 看到 第5章， 还没开始");
-        System.out.println(new Date() + " 看到 XXX， 还没开始");
+        System.out.println("Thu Jul 11 08:56:15 CST 2019" + " 看到 5.5.2， 还没开始");
+        //System.out.println(new Date() + " 看到 XXX， 还没开始");
 
     }
 }
